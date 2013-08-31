852940afb3edd7675a1b9aeee2e44603b36b402d
Fabien Campagne <fac2003@campagnelab.org>
2013-08-31 09:38:13 -0400

    Add generated, QueriesGenerated.java and dependencies to clean script.

--- clean.sh
+++ clean.sh
@@ -11,10 +11,11 @@ find . -name source_gen|xargs git rm -rf --cached
 find . -name source_gen.caches|xargs git rm -rf --cached
 find . -name classes_gen|xargs git rm -rf  --cached
 find . -name test_gen.caches |xargs git rm -rf  --cached
+find . -name dependencies |xargs git rm -rf  --cached
+find . -name generated |xargs git rm -rf  --cached
+find . -name QueriesGenerated.java |xargs git rm -rf  --cached
 
 
-
- 
 find . -name source_gen|xargs rm -fr
 find . -name source_gen.caches|xargs rm -fr
 find . -name classes_gen|xargs rm -fr
