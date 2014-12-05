7ef93c48dbf0822ad0191b1470670712955653b5
Manuele Simi <manuele.simi@campagnelab.org>
2014-12-05 13:57:40 -0500

    Remove test code from run-tests.

--- run-tests.sh
+++ run-tests.sh
@@ -2,10 +2,6 @@
 WORKING_DIR=`pwd`
 COPY_BACK_LOCATION=$1
 
-if [ -n "${COPY_BACK_LOCATION}" ]; then
-    COPY_BACK_LOCATION="$WORKING_DIR"
-fi
-
 NUM_OF_SUBMITTED_SUITES=0
 for fullfile in $(find ./solutions/*/classes_gen -name "*-remote-submit.ext.sh" -print);
 do
@@ -23,7 +19,7 @@ done
 REPORTS_FILES=$(ls -1 ${COPY_BACK_LOCATION} | wc -l)
 while [ "$REPORTS_FILES" -ne "$NUM_OF_SUBMITTED_SUITES" ]
 do
-      echo "Waiting for $COPY_BACK_LOCATION/plugins-reports"
+      echo "Waiting for $COPY_BACK_LOCATION"
       sleep 10
       REPORTS_FILES=$(ls -1 ${COPY_BACK_LOCATION} | wc -l)
 done
